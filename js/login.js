9f2fbfd566ee75cb187e8ed0431ad3846e352e30
AVARA33 <159586876+AVARA33@users.noreply.github.com>
2026-07-23 00:48:58 +0400

    Keep Personnel filters dark on hover

--- js/login.js
+++ js/login.js
@@ -21,7 +21,7 @@
    in js/storage.js and was not touched. */
 (function(){
   'use strict';
-  var BUILD_LABEL = 'ATSRS V271';
+  var BUILD_LABEL = 'ATSRS V272';
   var UPDATE_LABEL = 'Last Update: 23 Jul 2026';
   function lockBuildBadge(){
     var badge = document.getElementById('buildBadge') || document.querySelector('.build-badge');
