5346f8aec97c613a04c8c16b8fc904c845022760
AVARA33 <159586876+AVARA33@users.noreply.github.com>
2026-07-23 07:26:13 +0400

    Add work availability and maintenance status
    Release ATSRS V274 with personal work availability, corporate filters, and centrally controlled maintenance status.

--- js/login.js
+++ js/login.js
@@ -21,7 +21,7 @@
    in js/storage.js and was not touched. */
 (function(){
   'use strict';
-  var BUILD_LABEL = 'ATSRS V273';
+  var BUILD_LABEL = 'ATSRS V274';
   var UPDATE_LABEL = 'Last Update: 23 Jul 2026';
   function lockBuildBadge(){
     var badge = document.getElementById('buildBadge') || document.querySelector('.build-badge');
