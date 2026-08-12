2529eaaf584996c57235d523859e08c75887b2ae
AVARA33 <159586876+AVARA33@users.noreply.github.com>
2026-08-13 03:40:28 +0400

    Polish notification settings controls

--- js/boot-refresh.js
+++ js/boot-refresh.js
@@ -24,7 +24,7 @@
   }
   function loadV241(){
     lockBuildBadge();
-    loadAsset('link',{id:'atsrsNotificationsCss',rel:'stylesheet',href:'css/notifications.css?v=412'});
+    loadAsset('link',{id:'atsrsNotificationsCss',rel:'stylesheet',href:'css/notifications.css?v=422'});
     loadAsset('script',{id:'atsrsNotificationsJs',src:'js/notifications.js?v=384'});
   }
   function appIsOpen(){
