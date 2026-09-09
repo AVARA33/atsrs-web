e81f49ac511f65ccee9b2f9bede080eafcd484d8
AVARA33 <159586876+AVARA33@users.noreply.github.com>
2026-09-09 13:30:51 +0400

    Simplify document folder assignment

--- tests/documents-register-v5976.test.cjs
+++ tests/documents-register-v5976.test.cjs
@@ -12,7 +12,7 @@ assert.match(html, /id="documentSummaryValid"/);
 assert.match(html, /id="documentSummaryExpiring"/);
 assert.match(html, /id="documentSummaryNoExpiry"/);
 assert.match(html, /css\/documents-register-v5976\.css\?v=6102/);
-assert.match(html, /js\/app\.js\?v=6102/);
+assert.match(html, /js\/app\.js\?v=6103/);
 
 assert.match(html, /id="documentFolderBar"/);
 assert.match(html, /id="moveSelectedCertsFolder"/);
