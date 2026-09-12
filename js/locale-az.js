befb7dbd1bba7e42c71b7f2c3e438bd650e774e9
AVARA33 <159586876+AVARA33@users.noreply.github.com>
2026-09-13 01:05:06 +0400

    Revert "Add search-aware multi-role job filtering"
    This reverts commit 2b60e0c2dc83fcddebe9e7a19e0c77cc0619e522.

--- js/locale-az.js
+++ js/locale-az.js
@@ -269,7 +269,6 @@ window.ATSRS_AZ_MESSAGES = {
   "Search": "Axtarış",
   "Job title or role": "Vəzifə adı",
   "Role": "Vəzifə",
-  "roles selected": "vəzifə seçilib",
   "Region": "Region",
   "Country": "Ölkə",
   "City / location": "Şəhər / məkan",
--- js/route-feature-loader.js
+++ js/route-feature-loader.js
@@ -37,7 +37,7 @@
   }
 
   function loadJobs(){
-    return loadScript('js/jobs-prototype.js?v=6099');
+    return loadScript('js/jobs-prototype.js?v=6098');
   }
 
   function loadRecruiters(){
